3457c110335de90fddd453ac532564cca0cc54be
Gabriel Pastore <gabrielpastore2010@gmail.com>
2024-11-25 22:05:56 -0300

    fix: carrossel novo adicionado pela metade

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,17 +1,80 @@
-import styles from '../styles/components/Carousel.module.css';
 
 function Carousel(){
+
     return(
         <div>
-            <div className={styles.carouselContainer}>
-                <div className={styles.carouselText + " oswald-geral"}>BEM-VINDO</div>
-                <div className={styles.carousel}>
-                    <div className={styles.imgOne}></div>
-                    <div className={styles.imgTwo}></div>
-                    <div className={styles.imgThree}></div>
-                    <div className={styles.imgFour}></div>
+            <section class="carousel">
+
+                <div class="carousel__wrapper">
+            
+                    <div class="carousel__slide">
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/1/200/300');"></div>
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/1/200/300');"></div>
+                    </div>
+            
+                    <div class="carousel__slide">
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/2/200/300');"></div>
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/2/200/300');"></div>
+                    </div>
+            
+                    <div class="carousel__slide">
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/3/200/300');"></div>
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/3/200/300');"></div>
+                    </div>
+            
+                    <div class="carousel__slide">
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/4/200/300');"></div>
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/4/200/300');"></div>
+                    </div>
+            
+                    <div class="carousel__slide">
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/5/200/300');"></div>
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/5/200/300');"></div>
+                    </div>
+            
+                    <div class="carousel__slide">
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/picsum/200/300');">
+                      </div>
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/picsum/200/300');">
+                      </div>
+                    </div>
+            
+                    <!--#### repeat ####-->
+                    <div class="carousel__slide">
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/1/200/300');"></div>
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/1/200/300');"></div>
+                    </div>
+            
+                    <div class="carousel__slide">
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/2/200/300');"></div>
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/2/200/300');"></div>
+                    </div>
+            
+                    <div class="carousel__slide">
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/3/200/300');"></div>
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/3/200/300');"></div>
+                    </div>
+            
+                    <div class="carousel__slide">
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/4/200/300');"></div>
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/4/200/300');"></div>
+                    </div>
+            
+                    <div class="carousel__slide">
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/5/200/300');"></div>
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/5/200/300');"></div>
+                    </div>
+            
+                    <div class="carousel__slide">
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/picsum/200/300');">
+                      </div>
+                      <div class="carousel__image" style="background-image: url('https://picsum.photos/seed/picsum/200/300');">
+                      </div>
+                    </div>
+
                 </div>
-            </div>
+
+            </section>
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
+
 import styles from '../styles/pages/Home.module.css';
-import Carousel from '../components/Carousel';
 
 function Home(){
     return (
         <div>
-            <Carousel/>
             <div className={styles.title + " oswald-geral"}>PORTFOLIO</div>
         </div>
     )
