7f4e1c9fdb63cb9f59696f825793ac056f49af6d
Gabriel Pastore <79768975+GaPastore@users.noreply.github.com>
2024-12-04 15:38:29 -0300

    feat: faz testes com slider de itens

--- src/components/ContentSlider.js
+++ src/components/ContentSlider.js
@@ -1,7 +1,32 @@
-import { useState } from "react";
+import useState from 'react-usestateref';
 import styles from "../styles/components/ContentSlider.module.css"
 
 function ContentSlider(){
+
+    const [currentIndex, setCurrentIndex, indexRef] = useState(0);
+
+    const items = [
+        {
+            id: 1,
+            content: "teste1"
+        },
+        {
+            id: 2,
+            content: "teste2"
+        },
+        {
+            id: 3,
+            content: "teste3"
+        }
+    ]
+
+    const nextItem = () => {
+        setCurrentIndex((currentIndex + 1) % items.length);
+      };
+    
+    const prevItem = () => {
+        setCurrentIndex((currentIndex - 1 + items.length) % items.length);
+      };
 
     return(
         <div>
@@ -12,10 +37,10 @@ function ContentSlider(){
                     <div className={styles.sliderContent}></div>
                     <div className={styles.sliderContent}></div>
                 </div>
-                <div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                <div className={styles.buttonBox}>
+                    <div onClick={prevItem} className={styles.leftButton}></div>
+                    <div className={styles.openButton}></div>
+                    <div onClick={nextItem} className={styles.rightButton}></div>
                 </div>
             </div>
         </div>
