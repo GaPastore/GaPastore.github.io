10316e6414b788f282a9552440e92c9a230603eb
Gabriel Pastore <79768975+GaPastore@users.noreply.github.com>
2024-12-09 17:42:34 -0300

    fix: continua trabalhando no carrossel 2

--- src/components/ContentSlider.js
+++ src/components/ContentSlider.js
@@ -9,17 +9,18 @@ function ContentSlider() {
     const slide = document.getElementsByClassName(styles.sliderContent);
 
     function changePos() {
-        div[0].style.transform = "translateX(-" + ((currentIndex+1) * slide[0].style.width) +"px)"
+        div[0].style.transform = "translateX(calc(-" + ((indexRef.current) * (slide[0].clientWidth)) +"px - 10rem))"
     }
 
     const nextItem = () => {
-        setCurrentIndex((currentIndex + 1) % slide.length);
-        console.log(slide[0].style)
+        setCurrentIndex((currentIndex + 1) % ((slide.length / 2) + 1));
+        console.log(indexRef.current + 1)
         changePos()
       };
     
     const prevItem = () => {
-        setCurrentIndex((currentIndex - 1 + slide.length) % slide.length);
+        setCurrentIndex((currentIndex - 1 + slide.length) % (slide.length / 2));
+        console.log(indexRef.current + 1)
         changePos()
       };
 
@@ -27,10 +28,14 @@ function ContentSlider() {
         <div>
             <div className={styles.slider}>
                 <div className={styles.sliderBox}>
-                    <div className={styles.sliderContent}></div>
-                    <div className={styles.sliderContent}></div>
-                    <div className={styles.sliderContent}></div>
-                    <div className={styles.sliderContent}></div>
+                    <div className={styles.sliderContent} style={{backgroundColor: 'red'}}></div>
+                    <div className={styles.sliderContent} style={{backgroundColor: 'green'}}></div>
+                    <div className={styles.sliderContent} style={{backgroundColor: 'white'}}></div>
+                    <div className={styles.sliderContent} style={{backgroundColor: 'blue'}}></div>
+                    <div className={styles.sliderContent} style={{backgroundColor: 'red'}}></div>
+                    <div className={styles.sliderContent} style={{backgroundColor: 'green'}}></div>
+                    <div className={styles.sliderContent} style={{backgroundColor: 'white'}}></div>
+                    <div className={styles.sliderContent} style={{backgroundColor: 'blue'}}></div>
                 </div>
                 <div className={styles.buttonBox}>
                     <div onClick={prevItem} className={styles.leftButton}></div>
