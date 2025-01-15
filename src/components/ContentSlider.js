ae058302b8c787c0507b7d2f88dd9831f90aebd9
Gabriel Pastore <79768975+GaPastore@users.noreply.github.com>
2025-01-15 14:57:41 -0300

    style: sei la tenta fazer algo

--- src/components/ContentSlider.js
+++ src/components/ContentSlider.js
@@ -4,67 +4,82 @@ import styles from "../styles/components/ContentSlider.module.css"
 
 function ContentSlider() {
 
-    const [nIndex, setnIndex, indexRef] = useState(0);
+    const [nIndex, setnIndex] = useState(0);
+    const [disabled, setDisabled] = useState(false);
     
     const divRef = useRef();
 
     const slide = document.getElementsByClassName(styles.sliderContent);
-    const leftBt = document.getElementById(styles.leftButton);
-    const rightBt = document.getElementById(styles.rightButton);
-    const root = document.querySelector(':root')
-
-    var percent = 0
 
     const prevItem = () => {
 
-            console.log(divRef)
+        if(divRef.current.style.animationDirection == 'reverse'){
+            switch (nIndex){
+                case 2:
+                    divRef.current.style.animationDelay = '-4s'
+                    break
+                case 4:
+                    divRef.current.style.animationDelay = '-2s'
+                    break
+                case 6:
+                    divRef.current.style.animationDelay = '0s'
+                    break
+                case 8:
+                    divRef.current.style.animationDelay = '-8s'
+                    break
+                case 10:
+                    divRef.current.style.animationDelay = '-6s'
+                    break
+            }
+        }
 
-            percent = nIndex + 2
-            if(percent <= 0){
-                percent = 10
-            }
-            if(divRef.current.style.animationDirection == 'normal'){
-                divRef.current.style.animationDelay = '-' + percent + 's'
-            }
-            divRef.current.style.animationDirection = 'reverse'
-            divRef.current.style.animationPlayState = 'running'
-            divRef.current.disabled = true
+        divRef.current.style.animationDirection = 'reverse'
+        divRef.current.style.animationPlayState = 'running'
+        setDisabled(true)
+        console.log(nIndex)
+
+        setTimeout(() => {
+            divRef.current.style.animationPlayState = 'paused'
+            divRef.current.style.pointerEvents = 'auto'
             setnIndex((nIndex) => (nIndex - 2 + slide.length) % slide.length)
-
-            setTimeout(() => {
-                divRef.current.style.animationPlayState = 'paused'
-                divRef.current.disabled = false
-                
-                console.log(percent)
-            }, 2000)
-
-            console.log(divRef.current)
+            setDisabled(false)
+        }, 2000)
 
     };
 
     const nextItem = () => {
 
-        console.log(divRef)
+        if(divRef.current.style.animationDirection == 'normal'){
+            switch (nIndex){
+                case 2:
+                    divRef.current.style.animationDelay = '0s'
+                    break
+                case 4:
+                    divRef.current.style.animationDelay = '-2s'
+                    break
+                case 6:
+                    divRef.current.style.animationDelay = '-4s'
+                    break
+                case 8:
+                    divRef.current.style.animationDelay = '-6s'
+                    break
+                case 10:
+                    divRef.current.style.animationDelay = '-8s'
+                    break
+            }
+        }
 
-        percent = nIndex - 2
-        if(percent >= 10){
-            percent = 0
-        }
-        if(divRef.current.style.animationDirection == 'reverse'){
-            divRef.current.style.animationDelay = '-' + percent + 's'
-        }
         divRef.current.style.animationDirection = 'normal'
         divRef.current.style.animationPlayState = 'running'
-        divRef.current.disabled = true
-        setnIndex((nIndex) => (nIndex + 2) % slide.length)
+        setDisabled(true)
+        console.log(nIndex)
 
         setTimeout(() =>{
             divRef.current.style.animationPlayState = 'paused'
-            divRef.current.disabled = false
-            console.log(percent)
+            divRef.current.style.pointerEvents = 'auto'
+            setnIndex((nIndex) => (nIndex + 2) % slide.length)
+            setDisabled(false)
         }, 2000)
-
-        console.log(divRef.current)
 
     };
 
@@ -86,9 +101,9 @@ function ContentSlider() {
                     <div className={styles.sliderContent} style={{backgroundColor: 'purple'}}></div>
                 </div>
                 <div className={styles.buttonBox}>
-                    <div onClick={prevItem} id={styles.leftButton}></div>
+                    <div onClick={disabled ? () => {} : prevItem} id={styles.leftButton}></div>
                     <div id={styles.openButton}></div>
-                    <div onClick={nextItem} id={styles.rightButton}></div>
+                    <div onClick={disabled ? () => {} : nextItem} id={styles.rightButton}></div>
                 </div>
             </div>
         </div>
