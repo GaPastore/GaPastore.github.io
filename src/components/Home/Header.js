4a6ecc33f176a7390a41dc81b65d419d97a09682
Gabriel Pastore <gabrielpastore2010@gmail.com>
2025-03-04 12:56:59 -0300

    style: testa coisas animadas com scroll

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -3,12 +3,35 @@ import styles from '../../styles/components/Home/Header.module.css';
 
 function Header(){
 
+    function onLoad() {
+
+            console.log("teste")
+    
+            const observer = new IntersectionObserver(entries => {
+    
+                entries.forEach(entry => {
+                    if(entry.isIntersecting) {
+                        entry.target.classList.add(styles.onScreen)
+                        return
+                    }
+                })
+    
+            })
+    
+            const animElems = document.querySelectorAll('.animate')
+    
+            animElems.forEach((element) => observer.observe(element))
+
+    }
+
+    onLoad()
+
     return(
         <div>
             <div className={styles.introDiv}>
-                <div className={"title oswald-plus " + styles.thisTitle}>APRESENTAÇÃO</div>
-                <div className={styles.profileImg}></div>
-                <div className={styles.introText + " oswald-geral"}>
+                <div className={"title oswald-plus animate " + styles.thisTitle}>APRESENTAÇÃO</div>
+                <div className={"animate " + styles.profileImg}></div>
+                <div className={"oswald-geral animate " + styles.introText}>
                     Possuo conhecimento em áreas da tecnologia voltadas ao desenvolvimento de softwares, 
                     desenvolvimento de sites e design gráfico. Em 2014 tive a oportunidade de estudar na
                     escola SAGA, na qual fiz o curso Start e aprendi muito sobre design gráfico, 
