07d43eaf56e6ee8aa89732080d95ff85b62d49df
Gabriel Pastore <79768975+GaPastore@users.noreply.github.com>
2025-05-23 14:34:52 -0300

    style: corrige responsividade + melhora legibilidade dos textos

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -10,13 +10,13 @@ function Header(){
                     <div className={styles.profileImg}></div>
                 </div>
                 <div className={"oswald-geral " + styles.introText}>
-                    Possuo conhecimento em áreas da tecnologia voltadas ao desenvolvimento de softwares, 
-                    desenvolvimento de sites e design gráfico. Em 2014 tive a oportunidade de estudar na
+                    Possuo conhecimento em áreas da tecnologia voltadas ao desenvolvimento de <strong>softwares</strong>, 
+                    desenvolvimento de <strong>sites</strong> e <strong>design gráfico</strong>. Em 2014 tive a oportunidade de estudar na
                     escola SAGA, na qual fiz o curso Start e aprendi muito sobre design gráfico, 
-                    principalmente sobre edição audiovisual, edição de imagens e arte vetorial. Atualmente
-                    cursando Ciência da Computação na Universidade São Judas Tadeu, estou em contato com o
-                    mundo da programação desde 2018, quando iniciei o curso técnico de Informática juntamente 
-                    ao Ensino Médio na escola ENIAC.
+                    principalmente sobre <strong>audiovisual</strong>, edição de imagens e arte vetorial. Atualmente
+                    cursando <strong>Ciência da Computação</strong> na <strong>Universidade São Judas Tadeu</strong>, estou em contato com o
+                    mundo da programação desde 2018, quando iniciei o curso <strong>técnico de Informática</strong> juntamente 
+                    ao Ensino Médio no Colégio <strong>ENIAC</strong>.
                 </div>
             </div>
         </div>
